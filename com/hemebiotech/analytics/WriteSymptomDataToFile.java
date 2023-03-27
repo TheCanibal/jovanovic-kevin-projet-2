652ec57e92eb1c0852ce9bc4b4c07f8ce2c28a4d
TheCanibal <ziryadraenei@hotmail.fr>
2023-03-27 12:13:30 +0200

    Rajout des importations FileWriter, Exception et Iterator qui ont été supprimées automatiquement par Eclipse

--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,6 +1,9 @@
 package com.hemebiotech.analytics;
 
 import java.util.Map;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
 
 // WRITE YOUR CODE HERE
 public class WriteSymptomDataToFile implements ISymptomWriter {
