1a60276b6607ced3ba6b4d825a5e751bb82e6cda
TheCanibal <ziryadraenei@hotmail.fr>
2023-03-27 12:08:21 +0200

    Création de l'interface et de la classe qui serviront à écrire les symptômes dans un fichier

--- com/hemebiotech/analytics/ISymptomWriter.java
+++ com/hemebiotech/analytics/ISymptomWriter.java
@@ -2,11 +2,12 @@ package com.hemebiotech.analytics;
 
 import java.util.Map;
 
-
 /**
  * Anything the will write symptom data on a valid data output format
  *
  */
 public interface ISymptomWriter {
 // WRITE YOUR CODE HERE
+
+	void writeSymptoms(Map<String, Integer> symptoms);
 }
--- com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,8 +1,11 @@
 package com.hemebiotech.analytics;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Iterator;
 import java.util.Map;
 
 // WRITE YOUR CODE HERE
+public class WriteSymptomDataToFile implements ISymptomWriter {
+
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+
+	}
+}
